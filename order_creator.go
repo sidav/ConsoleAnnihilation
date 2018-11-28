81210e98ce0a42dc2cda3eb595d31a2ef67032f1
Unknown <sidav94@gmail.com>
2018-11-28 22:16:41 +0300

    Returns the nanolathe help order.

--- order_creator.go
+++ order_creator.go
@@ -1,12 +1,13 @@
 package main
 
 func issueDefaultOrderToUnit(u *pawn, m *gameMap, x, y int) {
-	// target = m.getUnitAtCoordinates(x, y)
-	//tBld := m.getBuildingAtCoordinates(x, y)
-	//if tBld != nil {
-	//	u.setOrder(&order{orderType: order_build, targetBuilding:tBld})
-	//	log.appendMessage(u.name + ": Helps nanolathing")
-	//	return
-	//}
+	target := m.getPawnAtCoordinates(x, y)
+	if target != nil {
+		if target.isBuilding() && target.currentConstructionStatus.isCompleted() == false {
+			u.setOrder(&order{orderType: order_build, targetBuilding: target})
+			log.appendMessage(u.name + ": Helps nanolathing")
+			return
+		}
+	}
 	u.setOrder(&order{orderType: order_move, x:x, y:y})
 }
