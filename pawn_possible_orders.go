116d6b614712bd8a3a3d66ae1a8016b4c941ce66
Vladislav Kovun <vkovun@phoenixit.ru>
2018-12-11 12:06:49 +0300

    Fixes the crash when selecting pawn without NanolatherInfo.

--- pawn_possible_orders.go
+++ pawn_possible_orders.go
@@ -1,11 +1,11 @@
 package main
 
 func (p *pawn) canConstructBuildings() bool {
-	return len(p.nanolatherInfo.allowedBuildings) > 0
+	return p.nanolatherInfo != nil && len(p.nanolatherInfo.allowedBuildings) > 0
 }
 
 func (p *pawn) canConstructUnits() bool {
-	return len(p.nanolatherInfo.allowedUnits) > 0
+	return p.nanolatherInfo != nil && len(p.nanolatherInfo.allowedUnits) > 0
 }
 
 func (p *pawn) canMove() bool {
