9f4dae34d8f2e537be44090b21a005e4ecf2d859
Unknown <sidav94@gmail.com>
2019-02-22 22:13:20 +0300

    WIP on art screens.

--- win_lose_screens.go
+++ win_lose_screens.go
@@ -3,48 +3,109 @@ package main
 import cw "TCellConsoleWrapper"
 
 var (
-	coreLogoArt = []string {
-"            *                ", 
-"         ##***##             ",
-"       ## ***** ##           ",
-"      #  *******  #          ",
-"     #  *********  #         ",
-"    #  ***********  #        ",
-"   #  *************  #       ",
-"  #  ***************  #      ",
-"  # ******** ******** #      ",
-"   ********   ********       ",
-"  ********     ********      ",
-"   ******       ******       ",
-"   ******       ******       ",
-"    *****       *****        ",
-"    *****       *****        ",
-"     ****#     #****         ",
-"     **** ##### ****         ",
-"      ***       ***          ",
-"      ***       ***          ",
-"       **       **           ",
-"       **       **           ",
-"        *       *            ",
+	coreWon = []string {
+"          *          ", 
+"       ##***##       ", 
+"     ## ***** ##     ", 
+"    #  *******  #    ", 
+"   #  *********  #   ", 
+"  #  ***********  #  ", 
+"  # ************* #  ", 
+" # *************** # ", 
+" #*****************# ", 
+" ******************* ", 
+"********** **********", 
+"*********   *********", 
+" *******     ******* ", 
+" ******       ****** ", 
+"  ****         ****  ", 
+"  ****         ****  ", 
+"  ****         ****  ", 
+"   ***         ***   ", 
+"   ***         ***   ", 
+"    **##     ##**    ", 
+"    **  #####  **    ", 
+"     *         *     ", 
+"     *         *     ",
+	}
+	coreLost = []string { 
+"          *          ",
+"       ##***##       ",
+"     ## ***** #      ",
+"    #  ******   #    ",
+"   #  ****** **  #   ",
+"     *****  ****  #  ",
+"  #  **    ****** #  ",
+" # *    * *  ***** # ",
+" #*** ***  ** *****# ",
+" **** **** *** ** ** ",
+"****** *** ***   ****",
+"   *** **   ** ** ***",
+" *  **         ***** ",
+" *** **       ****** ",
+"  ** *         **    ",
+"  ** *         *  *  ",
+"  **             **  ",
+"   ***         ***   ",
+"   ***         ***   ",
+"    **##     ##**    ",
+"    **   ## #  **    ",
+"     *         *     ",
+"     *         *     ",
 	}
 )
 
-func r_drawLogo(logo *[]string) {
-	for x:=0;x<len(*logo);x++{
-		for y:=0;y<len((*logo)[x]);y++ {
-			chr := (*logo)[x][y]
+func r_showTitleScreen() {
+	r_drawWinLogo()
+
+}
+
+func r_drawWinLogo() {
+	logo := &coreWon
+	cw.Clear_console()
+	cy := (cw.CONSOLE_HEIGHT - len(*logo))/2
+	cx := (cw.CONSOLE_WIDTH - len((*logo)[0]))/2
+	for y :=0; y <len(*logo); y++{
+		for x :=0; x <len((*logo)[y]); x++ {
+			chr := (*logo)[y][x]
 			switch chr {
 			case ' ':
 				continue
 			case '#':
 				cw.SetBgColor(cw.DARK_GREEN)
-				cw.PutChar(' ', y, x)
+				cw.PutChar(' ', x+cx, y+cy)
+				cw.SetBgColor(cw.BLACK)
+			case '*':
+				cw.SetFgColor(cw.GREEN)
+				cw.PutChar('*', x+cx, y+cy)
+			default:
+				cw.PutChar('?', x+cx, y+cy)
+			}
+		}
+	}
+	cw.Flush_console()
+}
+
+func r_drawLoseLogo() {
+	logo := &coreLost
+	cw.Clear_console()
+	cy := (cw.CONSOLE_HEIGHT - len(*logo))/2
+	cx := (cw.CONSOLE_WIDTH - len((*logo)[0]))/2
+	for y :=0; y <len(*logo); y++{
+		for x :=0; x <len((*logo)[y]); x++ {
+			chr := (*logo)[y][x]
+			switch chr {
+			case ' ':
+				continue
+			case '#':
+				cw.SetBgColor(cw.DARK_GRAY)
+				cw.PutChar(' ', x+cx, y+cy)
 				cw.SetBgColor(cw.BLACK)
 			case '*':
 				cw.SetFgColor(cw.DARK_GREEN)
-				cw.PutChar('*', y, x)
+				cw.PutChar('*', x+cx, y+cy)
 			default:
-				cw.PutChar('?', y, x)
+				cw.PutChar('?', x+cx, y+cy)
 			}
 		}
 	}
